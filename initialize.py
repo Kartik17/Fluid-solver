c1bcd02e97791e36a9bd81005b5c1f70c481912d
druther <harshit.chopra.19@gmail.com>
2015-09-29 15:40:43 +0530

    Added time stamps

--- initialize.py
+++ initialize.py
@@ -2,11 +2,14 @@
     the entire grid at t = 0'''
 
 import numpy as np
+import matplotlib as mp
 
 sizeX = 10
 sizeY = 10
+deltaTheta = 0.01         # Dimensionless time steps
 
 tempA = np.zeros(shape=(sizeX,sizeY))
-tempX = np.zeros(shape=(sizeX,sizeY))
+tempB = np.zeros(shape=(sizeX,sizeY))
+tempW1 = np.zeros(shape=(sizeX,sizeY))
 
-print tempA
+#print tempA
