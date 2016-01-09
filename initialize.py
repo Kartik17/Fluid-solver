91b64bf950e368f9cf1976acd88b99f554b3507c
druther <harshit.chopra.19@gmail.com>
2016-01-10 02:14:56 +0530

    Added variables for fluid B flow

--- initialize.py
+++ initialize.py
@@ -4,12 +4,33 @@
 import numpy as np
 import matplotlib as mp
 
+infi = 999999999999
+
 sizeX = 10
 sizeY = 10
 deltaTheta = 0.01         # Dimensionless time steps
 
 tempA = np.zeros(shape=(sizeX,sizeY))
 tempB = np.zeros(shape=(sizeX,sizeY))
+tempC = np.zeros(shape=(sizeX,sizeY))
 tempW1 = np.zeros(shape=(sizeX,sizeY))
+tempW2 = np.zeros(shape=(sizeX,sizeY))
 
-#print tempA
+# Constants
+
+Length = 10
+deltaX = Length/sizeX
+deltaY = Length/sizeY
+
+# For fluid B
+Vb = 0
+phi = 0.5     # Unsure of this value
+Na = 1        # Unsure of this value
+Peb = infi
+
+# Getting initial input over fluid B
+
+for j in range(sizeY):
+  tempB[0,j] = 1         # For constant input
+
+print tempB
